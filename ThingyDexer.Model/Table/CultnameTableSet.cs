caa23c88e35b0c8f7ee91a5aa537ecf671422d7b
ron dautzenberg <dutch.jafo@gmail.com>
2023-03-06 00:48:43 +0100

    refactoring of cult name generator UI + adding more templating options

--- ThingyDexer.Model/Table/CultnameTableSet.cs
+++ ThingyDexer.Model/Table/CultnameTableSet.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Linq;
+using ThingyDexer.Model.General;
 
 namespace ThingyDexer.Model.Table
 {
@@ -18,13 +19,53 @@ namespace ThingyDexer.Model.Table
             SomethingTable = new TextTable(_Randomizer, "Something", somethingTable);
         }
 
-        public (TableRowBase<string>? PrefixName, TableRowBase<string>? Name, TableRowBase<string>? PrefixSomething, TableRowBase<string>? Something) GenerateName(bool spiffy)
+        public (TableRowBase<string>? PrefixName, TableRowBase<string>? Name, TableRowBase<string>? PrefixSomething, TableRowBase<string>? Something) GenerateName(TableRowBase<string>? p1, TableRowBase<string>? n1, TableRowBase<string>? p2, TableRowBase<string>? n2, CultnameInputType cultnameInputType)
         {
             return (
-                    (spiffy ? PrefixTable.GetRandomItem() : null),
-                    NameTable.GetRandomItem(),
-                    PrefixNameTable.GetRandomItem(),
-                    SomethingTable.GetRandomItem()
+                    (cultnameInputType switch
+                    {
+                        CultnameInputType.TemplatePrefixNounOfTheAdjectiveNoun => p1 ?? PrefixTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheNoun => p1 ?? PrefixTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNoun => p1 ?? PrefixTable.GetRandomItem(),
+
+                        CultnameInputType.TemplatePrefixAdjectiveNoun => p1 ?? PrefixTable.GetRandomItem(),
+                        CultnameInputType.Custom => p1,
+                        _ => null,
+                    }),
+                    (cultnameInputType switch
+                    {
+                        CultnameInputType.TemplateNounOfTheAdjectiveNoun => n1 ?? NameTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheAdjectiveNoun => n1 ?? NameTable.GetRandomItem(),
+                        CultnameInputType.TemplateNounOfTheNoun => n1 ?? NameTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheNoun => n1 ?? NameTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNoun => n1 ?? NameTable.GetRandomItem(),
+
+                        CultnameInputType.Custom => n1,
+                        _ => null,
+                    }),
+                    (cultnameInputType switch
+                    {
+                        CultnameInputType.TemplateAdjectiveNoun => p2 ?? PrefixNameTable.GetRandomItem(),
+                        CultnameInputType.TemplateNounOfTheAdjectiveNoun => p2 ?? PrefixNameTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheAdjectiveNoun => p2 ?? PrefixNameTable.GetRandomItem(),
+
+                        CultnameInputType.TemplatePrefixAdjectiveNoun => p2 ?? PrefixNameTable.GetRandomItem(),
+                        CultnameInputType.Custom => p2,
+                        _ => null,
+                    }),
+                    (cultnameInputType switch
+                    {
+                        CultnameInputType.TemplateAdjectiveNoun => n2 ?? SomethingTable.GetRandomItem(),
+                        CultnameInputType.TemplateNounOfTheAdjectiveNoun => n2 ?? SomethingTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheAdjectiveNoun => n2 ?? SomethingTable.GetRandomItem(),
+                        CultnameInputType.TemplateNounOfTheNoun => n2 ?? SomethingTable.GetRandomItem(),
+                        CultnameInputType.TemplatePrefixNounOfTheNoun => n2 ?? SomethingTable.GetRandomItem(),
+
+                        CultnameInputType.TemplatePrefixAdjectiveNoun => n2 ?? SomethingTable.GetRandomItem(),
+
+                        CultnameInputType.Custom => n2,
+                        _ => null,
+                    })
                    );
         }
     }

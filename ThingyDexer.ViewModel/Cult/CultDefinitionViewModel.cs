fc1048304778ada258f210055330a166d194953c
ron dautzenberg <dutch.jafo@gmail.com>
2023-04-27 23:34:16 +0200

    improved validation & error messages

--- ThingyDexer.ViewModel/Cult/CultDefinitionViewModel.cs
+++ ThingyDexer.ViewModel/Cult/CultDefinitionViewModel.cs
@@ -6,7 +6,7 @@ namespace ThingyDexer.ViewModel.Cult
     public class CultDefinitionViewModel : ViewModelBase
     {
         private string _CultName = string.Empty;
-        [Required]
+        [Required(ErrorMessage ="Cult name is required")]
         public string CultName
         {
             get => _CultName;
@@ -14,7 +14,7 @@ namespace ThingyDexer.ViewModel.Cult
         }
 
         private string _Deity = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "Deity is required")]
         public string Deity
         {
             get => _Deity;
@@ -29,7 +29,7 @@ namespace ThingyDexer.ViewModel.Cult
         }
 
         private StatBlockType? _PrimaryFocus;
-        [Required]
+        [Required(ErrorMessage ="Primary focus is required")]
         public StatBlockType? PrimaryFocus
         {
             get => _PrimaryFocus;
@@ -37,7 +37,8 @@ namespace ThingyDexer.ViewModel.Cult
         }
 
         private int? _StartingFavour;
-        [Required]
+        [Required(ErrorMessage ="Need starting favour (300 or 500 recommended)")]
+        [Range(0, 1000, ErrorMessage ="Starting favour must be between 0 and 1000")]
         public int? StartingFavour
         {
             get => _StartingFavour;
@@ -45,7 +46,8 @@ namespace ThingyDexer.ViewModel.Cult
         }
 
         private int _InitialPower;
-        [Required]
+        [Required(ErrorMessage ="Initial power is required")]
+        [Range(0, 1000, ErrorMessage = "Initial power must be between 0 and 1000")]
         public int InitialPower
         {
             get => _InitialPower;
